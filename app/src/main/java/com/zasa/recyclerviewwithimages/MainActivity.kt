39b62f1c090ef0ce6635198c96c45a6372d33345
zansangeeth <zansangeeth17@gmail.com>
2022-04-21 23:07:50 +0530

    fixed recyclerview with multiple images

--- app/src/main/java/com/zasa/recyclerviewwithimages/MainActivity.kt
+++ app/src/main/java/com/zasa/recyclerviewwithimages/MainActivity.kt
@@ -5,28 +5,21 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val createWallpaper = createWallpapers()
-        rvRecyclerView.adapter = WallpaperAdapter(this, createWallpaper)
-
+        var myWallpaper =ImageDataSource().loadImageData()
+        rvRecyclerView.adapter = WallpaperAdapter(this, myWallpaper)
         rvRecyclerView.layoutManager = LinearLayoutManager(this)
 
-
     }
 
-    // method of returning wallpaper texts
-    private fun createWallpapers() : List<Wallpaper>{
-        val wallpapers = mutableListOf<Wallpaper>()
-        for (i in 1..10){
-            wallpapers.add(Wallpaper("wallpaper $i", R.drawable.image1))
-        }
-        return wallpapers
-    }
+
+
 
 
 }
